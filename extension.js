048cd86b38353870857af0ab06fd4b5b13bca0cf
rliang <ricardoliang@gmail.com>
2018-03-19 13:44:02 -0300

    Keep previous state.

--- extension.js
+++ extension.js
@@ -2,23 +2,34 @@ const Gio = imports.gi.Gio;
 const Main = imports.ui.main;
 const UPower = imports.ui.status.power.UPower;
 
-function check() {
+function getSettings() {
     const schema = Gio.SettingsSchemaSource.get_default()
        .lookup('org.gnome.desktop.interface', false);
-    if (!schema)
-       return;
-    const settings = new Gio.Settings({settings_schema: schema});
-    settings.set_boolean('enable-animations',
-        Main.panel.statusArea.aggregateMenu._power._proxy.State !== UPower.DeviceState.DISCHARGING);
+    if (schema)
+        return new Gio.Settings({settings_schema: schema});
 }
 
+function check() {
+    const settings = getSettings();
+    if (settings)
+        settings.set_boolean('enable-animations',
+            Main.panel.statusArea.aggregateMenu._power._proxy.State !== UPower.DeviceState.DISCHARGING);
+}
+
+let _previous_state;
 let _handle;
 
 function enable() {
+    const settings = getSettings();
+    if (settings)
+        _previous_state = settings.get_boolean('enable-animations');
     _handle = Main.panel.statusArea.aggregateMenu._power._proxy.connect('g-properties-changed', check);
     check();
 }
 
 function disable() {
+    const settings = getSettings();
+    if (settings)
+        settings.set_boolean('enable-animations', _previous_state);
     Main.panel.statusArea.aggregateMenu._power._proxy.disconnect(_handle);
 }
